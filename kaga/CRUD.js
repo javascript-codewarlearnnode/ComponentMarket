a9ef5ec59fd253787ab0d40e09e1c5d215c2b7ec
jlc7191 <44053726+jlc7191@users.noreply.github.com>
2019-05-22 18:26:44 +0800

    CRUD

---
+++ kaga/CRUD.js
@@ -0,0 +1,78 @@
+import React from 'react'
+class CRUD extends React.Component {
+    constructor(props, context) {
+        super(props, context)
+        this.state = {
+            wantUpdatesData: '',
+            updateToStatesData: '',
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            //新增使用者資料
+            const data = this.state.wantUpdatesData
+            try {
+                const res = await fetch('http://localhost:5555/cinemaCard', {
+                    method: 'POST',
+                    body: JSON.stringify(data),
+                    headers: new Headers({
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    }),
+                })
+                const jsonObject = await res.json()
+                await this.setState({ updateToStatesData: jsonObject }, () => {
+                    alert('資料已成功新增!')
+                    //把當初開啟的modal關掉
+                    this.handleModalClose()
+                })
+            } catch (e) {
+                console.log(e)
+            }
+
+            //讀取使用者資料
+            const response = await fetch('http://localhost:5555/cinemaCard', {
+                method: 'GET',
+                headers: new Headers({
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                }),
+            })
+            if (!response.ok) throw new Error(response.statusText)
+            const jsonObject = await response.json()
+            await this.setState({ total: jsonObject.total })
+        } catch (e) {
+            console.log(e)
+        } finally {
+        }
+    }
+    //更新使用者資料
+    handleClick = value => async () => {
+        try {
+            // data是改變後的資料
+            const data = { total: this.state.total + value }
+            const response = await fetch('http://localhost:5555/cinemaCard', {
+                method: 'PUT',
+                body: JSON.stringify(data),
+                headers: new Headers({
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                }),
+            })
+            const jsonObject = await response.json()
+            console.log(jsonObject)
+            // 資料庫改變完再回來拿原本的data改變state進而渲染整個頁面
+            await this.setState(data)
+        } catch (e) {
+            console.log(e)
+        } finally {
+        }
+    }
+
+    render() {
+        return
+    }
+}
+
+export default CRUD
